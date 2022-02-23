46aa56c4f34a656991191b034f10a6075926cc75
苏晟 <shengsu15@gmail.com>
2022-02-23 11:36:00 +0800

    Initial Commit

---
+++ BatteryBuddy/BatteryBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryBuddyApp.swift
+//  BatteryBuddy
+//
+//  Created by 苏晟 on 2022/2/23.
+//
+
+import SwiftUI
+
+@main
+struct BatteryBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
